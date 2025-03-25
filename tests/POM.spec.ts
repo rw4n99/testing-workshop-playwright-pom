24a14441518ee6ee52ecd174de13ed2f8b8ed1f9
Rowan Godfrey <rowangodfrey2@gmail.com>
2025-03-25 13:12:43 +0000

    Added fourth test case

--- tests/POM.spec.ts
+++ tests/POM.spec.ts
@@ -3,13 +3,13 @@ import LandingPage from "./pages/landingPage";
 import HoursPage from "./pages/hoursPage";
 import LeaveYearPage from "./pages/leaveYearPage";
 
-test(`Page object model happy path for second test`, async ({ page }): Promise<void> => {
+test(`Page object model happy path for Landing Page Test`, async ({ page }): Promise<void> => {
     const landingPage: LandingPage = new LandingPage();
     await landingPage.checkPageLoads(page);
     await landingPage.continueOn(page);
 });
 
-test(`Page object model happy path for third test`, async ({ page }): Promise<void> => {
+test(`Page object model happy path for Hours Input Page test`, async ({ page }): Promise<void> => {
 
     const hoursPage: HoursPage = new HoursPage();
     await hoursPage.checkPageLoads(page);
@@ -17,10 +17,10 @@ test(`Page object model happy path for third test`, async ({ page }): Promise<vo
     await hoursPage.continueOn(page);
 });
 
-test(`Page object model happy path for fourth test`, async ({ page }): Promise<void> => {
+test(`Page object model happy path for Leave Year Page test`, async ({ page }): Promise<void> => {
 
     const leaveYearPage: LeaveYearPage = new LeaveYearPage();
     await leaveYearPage.checkPageLoads(page);
-    // await hoursPage.checkYes(page);
-    // await hoursPage.continueOn(page);
+    await leaveYearPage.enterDate(page);
+    await leaveYearPage.continueOn(page);
 });
--- tests/pages/leaveYearPage.ts
+++ tests/pages/leaveYearPage.ts
@@ -3,22 +3,39 @@ import LeaveYearPage_content from '../content/leaveYearPage_content';
 
 class LeaveYearPage {
   private readonly title: string;
-//   private readonly startButton: string;
+    private readonly inputDay: string;
+    
+
   constructor() {
     this.title = '.govuk-fieldset__heading';
-    //this.startButton = 'text=Start now';
+    this.inputDay = '.gem-c-input'
   }
 
   async checkPageLoads(page: Page): Promise<void> {
     await page.goto('https://www.gov.uk/calculate-your-holiday-entitlement/y/irregular-hours-and-part-year');
     await Promise.all([
       expect(page.locator(this.title)).toHaveText(LeaveYearPage_content.pageTitle),
-    //   expect(page.locator(this.startButton)).toBeVisible(),
     ]);
   }
   
-//   async continueOn(page: Page): Promise<void> {
-//     await page.click(this.startButton);
-//   }
+  async enterDate(page: Page): Promise<void> {
+
+    await page.getByLabel("Day").fill("01"),
+    await page.getByLabel("Month").fill("02"),
+    await page.getByLabel("Year").fill("2025")
+        await Promise.all([
+         expect(page.getByLabel("Day")).toHaveValue('01'),
+         expect(page.getByLabel("Month")).toHaveValue('02'),
+         expect(page.getByLabel("Year")).toHaveValue('2025'),
+    ])
+  }
+
+  async continueOn(page: Page): Promise<void> {
+    await page.getByRole('button', { name: 'Continue' }).click();
+    await expect(page).toHaveURL('https://www.gov.uk/calculate-your-holiday-entitlement/y/irregular-hours-and-part-year/2025-02-01')
+  }
+
 }
 export default LeaveYearPage;
+
+//01/02/2025
