daf213985fa479a7e1b3e6f415f987aeef138ec6
bellalevene1 <isabella.levene@solirius.com>
2025-03-25 14:06:51 +0000

    continue button works

--- tests/pages/payPeriod.ts
+++ tests/pages/payPeriod.ts
@@ -1,35 +1,29 @@
 import { Page, expect } from '@playwright/test';
-import PayPeriod_content from '../content/payPeriod_content'
+import PayPeriod_content from '../content/payPeriod_content';
 
 class payPeriod {
   private readonly title: string;
-    private readonly inputHours: string;
-    
+  private readonly inputHours: string;
 
   constructor() {
     this.title = 'h1.govuk-label-wrapper';
-    this.inputHours = '.gem-c-input'
+    this.inputHours = '.gem-c-input';
   }
 
   async checkPageLoads(page: Page): Promise<void> {
     await page.goto('https://www.gov.uk/calculate-your-holiday-entitlement/y/irregular-hours-and-part-year/2025-02-01');
-    await Promise.all([
-      expect(page.locator(this.title)).toHaveText(PayPeriod_content.pageTitle),
-    ]);
+    await expect(page.locator(this.title)).toHaveText(PayPeriod_content.pageTitle);
   }
-  
-  async enterDate(page: Page): Promise<void> {
 
-    await page.fill(this.inputHours, '40'),
-        await Promise.all([
-         expect(page.locator('.gem-c-input')).toHaveText('40'),
-    ])
+  async enterDate(page: Page): Promise<void> {
+    await page.fill(this.inputHours, '40');
+    await expect(page.locator(this.inputHours)).toHaveValue('40');
   }
 
   async continueOn(page: Page): Promise<void> {
     await page.getByRole('button', { name: 'Continue' }).click();
-    await expect(page).toHaveURL('https://www.gov.uk/calculate-your-holiday-entitlement/y/irregular-hours-and-part-year/2025-02-01/40.0')
+    await expect(page).toHaveURL('https://www.gov.uk/calculate-your-holiday-entitlement/y/irregular-hours-and-part-year/2025-02-01/40.0');
   }
-
 }
+
 export default payPeriod;
