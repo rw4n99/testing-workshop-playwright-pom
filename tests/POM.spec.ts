a2703dc725dc91f842576f9637892eb8ae5bc703
Rowan Godfrey <rowangodfrey2@gmail.com>
2025-03-25 11:53:52 +0000

    Added leave year page

--- tests/POM.spec.ts
+++ tests/POM.spec.ts
@@ -1,12 +1,12 @@
 import {test} from "@playwright/test";
 import LandingPage from "./pages/landingPage";
 import HoursPage from "./pages/hoursPage";
+import LeaveYearPage from "./pages/leaveYearPage";
 
 test(`Page object model happy path for second test`, async ({ page }): Promise<void> => {
     const landingPage: LandingPage = new LandingPage();
     await landingPage.checkPageLoads(page);
     await landingPage.continueOn(page);
-    // Continue here!
 });
 
 test(`Page object model happy path for third test`, async ({ page }): Promise<void> => {
@@ -15,6 +15,12 @@ test(`Page object model happy path for third test`, async ({ page }): Promise<vo
     await hoursPage.checkPageLoads(page);
     await hoursPage.checkYes(page);
     await hoursPage.continueOn(page);
-    // Continue here!
 });
 
+test(`Page object model happy path for fourth test`, async ({ page }): Promise<void> => {
+
+    const leaveYearPage: LeaveYearPage = new LeaveYearPage();
+    await leaveYearPage.checkPageLoads(page);
+    // await hoursPage.checkYes(page);
+    // await hoursPage.continueOn(page);
+});
